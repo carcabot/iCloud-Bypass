0b1c22a51ecf2648aa75603ae2288567292ca89b
carcabot <carcabot@carcabot.ro>
2014-06-04 17:36:33 +0300

    Update deviceActivation.php

--- deviceActivation.php
+++ deviceActivation.php
@@ -92,43 +92,7 @@ HTML;
 }
 exit;
 break;
-case 'deviceActivation':
-header("Content-type: text/html");
-echo <<<HTML
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html
-  xmlns="http://www.apple.com/itms/" lang="en">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="keywords" content="iTunes Store" />
-    <meta name="description" content="iTunes Store" />
-    <title>iPhone Activation</title>
-    <link href="http://static.ips.apple.com/ipa_itunes/stylesheets/shared/common-min.css" charset="utf-8" rel="stylesheet" />
-    <link href="null/deviceservices/stylesheets/styles.css" charset="utf-8" rel="stylesheet" />
-    <link href="http://static.ips.apple.com/ipa_itunes/stylesheets/pages/IPAJingleEndPointErrorPage-min.css" charset="utf-8" rel="stylesheet" />
-    <script id="protocol" type="text/x-apple-plist">
-      <plist version="1.0">
-        <dict>
-          <key>iphone-activation</key>
-          <dict>
-            <key>ack-received</key>
-            <true/>
-            <key>show-settings</key>
-            <true/>
-          </dict>
-        </dict>
-      </plist>
-    </script>
-    <script>
-var protocolElement = document.getElementById("protocol");
-var protocolContent = protocolElement.innerText;iTunes.addProtocol(protocolContent);
-</script>
-  </head>
-  <body></body>
-</html>
-HTML;
-exit;
-break;
+
 case 'checkUnbrickHealth':
 header("Content-Type: application/xml");
 echo '<plist version="1.0">
